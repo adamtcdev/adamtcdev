d7954092c9195aaf2a2129ea0ead86d34062c09a
Trương Quang Tùng <157731913+adamtcdev@users.noreply.github.com>
2026-01-14 23:55:53 +0700

    Update install script to use no-install-recommends

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 wget http://archive.ubuntu.com/ubuntu/pool/main/u/ubuntu-meta/ubuntu-desktop-minimal_1.560_amd64.deb \
-&& sudo apt install ./ubuntu-desktop-minimal_1.560_amd64.deb \
+&& sudo apt install --no-install-recommends ./ubuntu-desktop-minimal_1.560_amd64.deb \
 && rm ubuntu-desktop-minimal_1.560_amd64.deb
